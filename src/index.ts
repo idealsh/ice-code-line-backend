c4a24a97058ae5163b98e5cc2a28d8d149df95df
idealsh <141259309+idealsh@users.noreply.github.com>
2024-07-14 15:36:51 +0700

    verify jwt token

--- src/index.ts
+++ src/index.ts
@@ -23,17 +23,55 @@ server.register(async (childServer) => {
     }
   });
 
-  childServer.get("/api/me", async (req, res) => {
-    const { id } = req.user as { id: string };
+  type LoginToken = {
+    sub?: string;
+    aud?: string;
+    jti?: string;
+    ip?: string;
+  };
 
-    const result = await sql`SELECT * FROM seniors WHERE id = ${id}`;
+  childServer.get("/api/login", async (req, res) => {
+    const { sub: id, aud, jti: nonce, ip } = req.user as LoginToken;
+
+    if (aud !== "login") {
+      res.status(400);
+      return { message: "Token is not for log in" };
+    }
+    if (id === undefined || nonce === undefined || ip === undefined) {
+      res.status(400);
+      return { message: "Login token is incomplete" };
+    }
+
+    const result = await sql`SELECT * FROM sophomores WHERE id = ${id}`;
     const me = result[0];
 
     if (me === undefined) {
-      throw new Error(`Could not find info for ${id}`);
-    } else {
-      return me;
+      res.status(500);
+      return {
+        ref: "SOPHOMORE_INFO_NOT_FOUND",
+        message: `Could not find information for ${id}`
+      };
     }
+
+    const loginEvent = {
+      ip,
+      userId: id,
+      eventName: "login",
+      nonce
+    };
+    await sql`INSERT INTO events ${sql(loginEvent)} ON CONFLICT ON CONSTRAINT events_nonce_key DO NOTHING`;
+
+    const accessToken = childServer.jwt.sign(
+      {
+        me
+      },
+      {
+        sub: id,
+        aud: "access"
+      }
+    );
+
+    return { accessToken };
   });
 });
 
